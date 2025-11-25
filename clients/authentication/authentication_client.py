677e8c12c32f4617d207313500c7b6ce09de770d
MaximYugin <102412402+MaximYugin@users.noreply.github.com>
2025-11-25 20:51:48 +0300

    даление артефактов покрытия

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -6,6 +6,7 @@ from clients.api_client import APIClient
 from clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
 from clients.public_http_builder import get_public_http_client
 from tools.routes import APIRoutes  # Импортируем enum APIRoutes
+from clients.api_coverage import tracker  # Импортируем трекер из api_coverage.py
 
 # Старые модели с использованием TypedDict были удалены
 
@@ -15,6 +16,7 @@ class AuthenticationClient(APIClient):
     """
 
     @allure.step("Authenticate user")
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/login")
     def login_api(self, request: LoginRequestSchema) -> Response:
         """
         Метод выполняет аутентификацию пользователя.
@@ -29,6 +31,7 @@ class AuthenticationClient(APIClient):
         )
 
     @allure.step("Refresh authentication token")
+    @tracker.track_coverage_httpx(f"{APIRoutes.AUTHENTICATION}/refresh")
     def refresh_api(self, request: RefreshRequestSchema) -> Response:
         """
         Метод обновляет токен авторизации.
